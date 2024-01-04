f43a76e843d9e3779feb47e013394b7bc2407486
h3062 <152046673+h3062@users.noreply.github.com>
2024-01-05 02:49:59 +0800

    control.php

--- control.php
+++ control.php
@@ -72,5 +72,41 @@ function sendOrderStatusNotification($userId, $orderId, $newStatus) {
     // ...
 }
 
-// 調用通知函式
-sendOrderStatusNotification($userId, $orderId, $newStatus);
+
+//下一步(不確定)
+
+class UserController {
+    private $users;
+
+    public function __construct() {
+        // 建立一個使用者資料陣列，模擬資料庫中的使用者資料
+        $this->users = [
+            ['id' => 1, 'username' => 'user1', 'password' => 'password1'],
+            ['id' => 2, 'username' => 'user2', 'password' => 'password2'],
+        ];
+    }
+
+    public function login($username, $password) {
+        // 透過迴圈檢查提供的帳號和密碼是否與陣列中的某個使用者匹配
+        foreach ($this->users as $user) {
+            if ($user['username'] == $username && $user['password'] == $password) {
+                // 登入成功，將使用者的 ID 存入 session 中
+                $_SESSION['user_id'] = $user['id'];
+                return true;
+            }
+        }
+
+        // 登入失敗
+        return false;
+    }
+
+    public function register($username, $password) {
+        // 假設簡單註冊，直接將新的帳號和密碼加入使用者陣列
+        $newUser = ['id' => count($this->users) + 1, 'username' => $username, 'password' => $password];
+        $this->users[] = $newUser;
+        return true;
+    }
+
+    // 未顯示的部分可能包含其他功能，例如修改密碼、取得使用者資訊等等
+}
+
