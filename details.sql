b011677ddad26482249367706bb3a48aa5400a20
s110213049 <apple84621@gmail.com>
2024-01-04 19:31:34 +0800

    update details.sql

--- details.sql
+++ details.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2024-01-02 19:29:21
+-- 產生時間： 2024-01-04 12:29:53
 -- 伺服器版本： 10.4.28-MariaDB
 -- PHP 版本： 8.2.4
 
@@ -33,26 +33,6 @@ CREATE TABLE `details` (
   `goodID` int(10) NOT NULL,
   `purNum` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- 已傾印資料表的索引
---
-
---
--- 資料表索引 `details`
---
-ALTER TABLE `details`
-  ADD PRIMARY KEY (`detailID`);
-
---
--- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
---
-
---
--- 使用資料表自動遞增(AUTO_INCREMENT) `details`
---
-ALTER TABLE `details`
-  MODIFY `detailID` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
