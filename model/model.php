227a9cfbbd5d91f689616a3898467afffffbc678
PeiYingYu <137051616+PeiYingYu3003@users.noreply.github.com>
2024-01-08 14:24:41 +0800

    Add files via upload

--- model/model.php
+++ model/model.php
@@ -32,4 +32,51 @@ function confirm_OrderList(){
 	}
 	return $rows;
 }
+
+function registerUser($account, $password, $role) {
+    global $db;
+    $sql = "insert into user (account, password, role) values (?, ?, ?)";
+    $stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "ssi", $account, $password, $role);
+    mysqli_stmt_execute($stmt);
+}
+
+function registerUser_client($account,$password,$ClinetName,$ClinetAddress){
+    global $db;
+    registerUser($account, $password, 0); // 將帳號密碼資料加入 user 資料庫
+    $userID = mysqli_insert_id($db);
+    
+    // 將對應用戶加入clients資料庫中
+    $sql = "insert into clients (`userID`, `clientName`, `clientAddress`) values (?, ?, ?)";
+    $stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "iss", $userID, $ClinetName, $ClinetAddress);
+    mysqli_stmt_execute($stmt);
+    return "finish";
+}
+
+// 新商家註冊
+function registerUser_shop($account,$password,$ShopName){
+    global $db;
+    registerUser($account, $password, 1); // 將帳號密碼資料加入 user 資料庫
+    $userID = mysqli_insert_id($db);
+    $shopStar = 0;
+    // 將對應用戶加入clients資料庫中
+    $sql = "insert into shops (`userID`, `shopName`,`shopStar`) values (?, ?, ?)";
+    $stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "isi", $userID, $ShopName, $shopStar);
+    mysqli_stmt_execute($stmt);
+    return "finish";
+}
+
+function isExist($account) {
+    global $db;
+    
+    $sql = "SELECT * FROM users WHERE account = ?";
+    $stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $account);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    return mysqli_num_rows($result) > 0;
+}
 ?>
