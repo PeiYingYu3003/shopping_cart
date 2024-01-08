ef1d3c574a4a5fd332f53bb7edc24f769b7a8900
PeiYingYu <137051616+PeiYingYu3003@users.noreply.github.com>
2024-01-08 15:31:20 +0800

    Add files via upload

--- model/model.php
+++ model/model.php
@@ -21,7 +21,7 @@ function login($acc, $pwd) {
 // 確認有哪些訂單
 function confirm_OrderList(){
 	global $db;
-	$sql = "select * from orders,clients,shops where orders.clientID=clients.clientID and orders.shopID = shops.shopID;";
+	$sql = "select * from orders,clients,shops where  orderStatus>0 and orders.clientID=clients.clientID and orders.shopID = shops.shopID;";
 	$stmt = mysqli_prepare($db, $sql); 
 	mysqli_stmt_execute($stmt); //執行SQL
 	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
@@ -59,7 +59,7 @@ function registerUser_shop($account,$password,$ShopName){
     global $db;
     registerUser($account, $password, 1); // 將帳號密碼資料加入 user 資料庫
     $userID = mysqli_insert_id($db);
-    $shopStar = 0;
+    $shopStar = null;
     // 將對應用戶加入clients資料庫中
     $sql = "insert into shops (`userID`, `shopName`,`shopStar`) values (?, ?, ?)";
     $stmt = mysqli_prepare($db, $sql);
